68fd560d739d69bf34ac328a7459e429b7948d18
paulofranklins <paulofranklinc@gmail.com>
2026-01-08 14:54:22 -0800

    initial commit

---
+++ src/main/java/org/duckdns/todosummarized/TodoSummarizedApplication.java
@@ -0,0 +1,13 @@
+package org.duckdns.todosummarized;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoSummarizedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoSummarizedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/duckdns/todosummarized/TodoSummarizedApplicationTests.java
@@ -0,0 +1,13 @@
+package org.duckdns.todosummarized;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoSummarizedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
