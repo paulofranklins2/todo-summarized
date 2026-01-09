e00a525c8be9650f7c9e7e8ed7e73c355db7c295
paulofranklins <paulofranklinc@gmail.com>
2026-01-08 20:45:45 -0800

    added user entity

---
+++ src/main/java/org/duckdns/todosummarized/domains/entity/User.java
@@ -0,0 +1,91 @@
+package org.duckdns.todosummarized.domains.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.duckdns.todosummarized.domains.enums.Role;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.Clock;
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+@ToString(exclude = "password")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @EqualsAndHashCode.Include
+    private UUID id;
+    @Column(nullable = false, unique = true, updatable = false)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+    @Column(nullable = false)
+    private boolean enabled = true;
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+    @Column(nullable = false)
+    private boolean accountNonExpired = true;
+    @Column(nullable = false)
+    private boolean accountNonLocked = true;
+    @Column(nullable = false)
+    private boolean credentialsNonExpired = true;
+
+    @PrePersist
+    void prePersist() {
+        var now = LocalDateTime.now(Clock.systemUTC());
+        this.createdAt = now;
+        this.updatedAt = now;
+    }
+
+    @PreUpdate
+    void preUpdate() {
+        this.updatedAt = LocalDateTime.now(Clock.systemUTC());
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return accountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return accountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return credentialsNonExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+}
